132adf366a61ecf9b4f60b7e938b1f05fd2adefe
Experiment 626 <rcourt@live.com.au>
2021-02-06 18:03:41 +1100

    connected .json with api js file

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -1,3 +1,19 @@
+const fs = require('fs');
+//const dbJSON = require('./test.json')
+const data = fs.readFileSync('./db/db.json');
+const notes = JSON.parse(data);
+console.log(notes);
+
 module.exports = (app) => {
-    console.log('agape love');
+    app.get('/api/notes', (req, res) => res.json.notes);
+    // app.get('/api/notes', (req, res) => {
+    //     //should read the 'db.json' file and return all saved notes as JSON
+    //     res.sendFile(path.join(__dirname, '../db/db.json'));
+    // });
+
+    // app.post('/api/notes', (req, res) => {
+    //     //should receive a new note to save on the request body, add it to the `db.json` file, and then return the new note to the client. 
+    //     //You'll need to find a way to give each note a unique id when it's saved (look into `npm` packages that could do this for you).
+    //     res.sendFile(path.join(__dirname, '../public/index.html'));
+    // });
 }
--- Develop/server.js
+++ Develop/server.js
@@ -2,14 +2,18 @@
 
 const express = require('express');
 const chalk = require('chalk');
-
+const htmlRoutes = require('./routes/htmlRoutes');
+const apiRoutes = require('./routes/apiRoutes');
 // EXPRESS CONFIGURATION
 const app = express();
-
+// whatever is specified in the user environment or port 3000
 const PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static('public'));
+//app.use('/api', apiRoutes);
+//app.use('/', htmlRoutes);
 
 // ROUTER
 
